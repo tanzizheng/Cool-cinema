4b9c345d2508dfd4ed84daa8bba5993b0fef49a6
15856027@qq.com <tan>
2019-09-24 17:29:46 +0800

    'b'

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main">
+      <Header title="钩钩影院"/>
+      <tabBar/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+export default {
+    name:'Cinema',
+    components:{
+        Header,
+        TabBar
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main">
+      <Header title="mi钩钩"/>
+      <tabBar/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+export default {
+    name:'Mine',
+    components:{
+        Header,
+        TabBar
+    }
+}
+</script>
+
+<style>
+
+</style>
