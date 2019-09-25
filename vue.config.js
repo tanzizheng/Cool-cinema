86b30d44ebd30fa3e5f0996d506523ba443719c4
15856027@qq.com <tan>
2019-09-25 20:28:21 +0800

    setdata created

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports={
+    //开发环境配置
+    devServer:{
+        proxy:{
+            '/api':{
+                //反响代理目标
+                target:'http://39.97.33.178',
+                changeOrigin:true    //是否改变代理地址
+            }
+        }
+    }
+}
